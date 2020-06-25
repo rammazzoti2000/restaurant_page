a90193640b3c5a92d3f922659551b4c5c82923a1
Alexandru <rammazzoti2000@gmail.com>
2020-06-25 10:22:53 +0200

    add style and images

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
-console.log('It works! Great, move on.');
+import './style.css';
+import AngryBird from './vegetable-salad-on-white-plate.jpg'
+
+function component() {
+    const element = document.createElement('div');
+
+    element.innerHTML = 'Hello webpack',
+    element.classList.add('hello');
+
+    const myImage = new Image();
+    myImage.src = AngryBird;
+
+    element.appendChild(myImage);
+
+    return element;
+  }
+
+  document.body.appendChild(component());
--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,24 @@ const path = require('path');
 module.exports = {
   entry: './src/index.js',
   output: {
-    filename: 'main.js',
+    filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader',
+        ],
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [
+          'file-loader',
+        ],
+      },
+    ],
   },
 };
