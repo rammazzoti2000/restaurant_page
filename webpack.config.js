e4230787496a558a8ba4f4cbc9cb873413b5c525
Alexandru Bangau <rammazzoti2000@gmail.com>
2020-06-26 13:11:57 +0200

    Small screen breakpoint, change js file to main.js

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = {
   entry: './src/index.js',
   output: {
-    filename: 'bundle.js',
+    filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
   },
   module: {
