9c7b8b149627a500e291b98a74f00d0a883edb51
Alexandru Bangau <rammazzoti2000@gmail.com>
2020-06-26 10:48:20 +0200

    Clean up the /dist folder with clean-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,11 @@
 const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
+  plugins: [
+    new CleanWebpackPlugin(),
+  ],
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
